5d2149286087ca07aacdab7e6d75b3d33075ad9b
Alireza-Office <mkalireza.ir@gmail.com>
2023-10-20 12:19:09 +0330

    S2: E14 updated

---
+++ S02-Variables-Data-Types-Operators-And-More/14-NaN-Infinity/script.js
@@ -0,0 +1,12 @@
+//NaN => Not a Number
+console.log("salam" / 5);
+console.log("salam" * 5);
+console.log("salam" - 5);
+
+//Infinity
+console.log(10 / 0);
+
+const a = Infinity;
+const b = NaN;
+console.log(a, typeof a);
+console.log(b, typeof b);
