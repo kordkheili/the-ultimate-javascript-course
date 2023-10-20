fb1b3ae12ae552f6f729b4e8aac0f11c6032a238
Alireza-Office <mkalireza.ir@gmail.com>
2023-10-20 12:02:00 +0330

    S2: E13 updated

--- S02-Variables-Data-Types-Operators-And-More/13-Type-Coercion/script.js
+++ S02-Variables-Data-Types-Operators-And-More/13-Type-Coercion/script.js
@@ -1,1 +1,9 @@
 //!Type Coercion In Javascript
+console.log(10 + "10");
+console.log(10 + "alireza");
+console.log(10 - "10");
+console.log(10 - "alireza"); //NaN
+console.log(10 * "alireza"); //NaN
+console.log(10 / "alireza"); //NaN
+console.log(true + 1);
+console.log(false - 1);
