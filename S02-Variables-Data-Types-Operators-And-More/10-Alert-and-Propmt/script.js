bdd40b7b0a3c9ba00c293ebf8f4ca6ecbb6d17b9
Alireza-Office <mkalireza.ir@gmail.com>
2023-10-20 11:10:31 +0330

    S2: E10,E11,E12,E13,E14 added

---
+++ S02-Variables-Data-Types-Operators-And-More/10-Alert-and-Propmt/script.js
@@ -0,0 +1,11 @@
+// ! alert
+// console.log("salam");
+// alert("helloooo");
+// console.log("khodafez");
+
+//propmt
+const input = prompt("لطفا نام خود را وارد نمایید...");
+console.log(input + " عزیز خوش آمدی");
+
+const age = prompt("لطفا سن خود را وارد نمایید");
+console.log(age, typeof age);
---
+++ S02-Variables-Data-Types-Operators-And-More/12-Type-Conversion/script.js
@@ -0,0 +1,18 @@
+//!Type Conversion In Javascript
+
+//*Convert to Number
+// const age = prompt("لطفا سال تولد خود را وارد نمایید");
+// const result = Number(age) + 18;
+// const result = +age + 18;
+// console.log(result);
+
+//*Convert to String
+// const x = 100;
+// console.log(x, typeof x);
+// const x_string = String(x);
+// console.log(x_string, typeof x_string);
+
+//*Convert to Boolean
+const isMale = 1;
+const isMale_boolean = Boolean(isMale);
+console.log(isMale_boolean, typeof isMale_boolean);
