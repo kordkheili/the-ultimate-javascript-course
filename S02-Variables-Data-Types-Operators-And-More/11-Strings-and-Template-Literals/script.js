21e3b1bd0e08a8eb825a6f28a214f7cbffc1160e
Alireza-Office <mkalireza.ir@gmail.com>
2023-10-20 11:38:35 +0330

    S2: E11 updated

---
+++ S02-Variables-Data-Types-Operators-And-More/11-Strings-and-Template-Literals/script.js
@@ -0,0 +1,9 @@
+// const firstName = "Alireza";
+// const lastName = "Mohammadi";
+// console.log(firstName + " " + lastName);
+
+const firstName = prompt("firstname");
+const lastName = prompt("lastname");
+// console.log("Hello, My name is " + firstName + " " + lastName);
+// console.log("Hello, My name is " + firstName + " " + lastName);
+console.log(`Hello, My name is ${firstName} ${lastName}`);
