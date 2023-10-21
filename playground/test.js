91d11a3ad98ce02a128c0490bbd5bde00e3bbce9
Alireza-Office <mkalireza.ir@gmail.com>
2023-10-21 11:43:35 +0330

    folder renamed

--- playground/test.js
+++ playground/test.js
@@ -4,3 +4,27 @@
     Multi-Line
     comment 
 */
+if (condition) {
+  //  block of code to be executed if the condition is true
+}
+
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
+
+if (true) {
+  const foo = "foo";
+  console.log(foo); // "foo"
+}
+
+console.log(foo); // Uncaught ReferenceError: foo is not defined
