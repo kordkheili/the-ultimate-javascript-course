707da8bc66e42b4e2e235b6e6ea13a894ab92cff
Alireza-Office <mkalireza.ir@gmail.com>
2023-11-06 15:55:10 +0330

    Module Introduction Window Object in JavaScript Document Object Model (DOM)

---
+++ sessions/T100-Working with DOM/S02-Document-Object-Model-DOM/script.js
@@ -0,0 +1,5 @@
+// window.alert("Hello!");
+// window.console.log("Welcome to Websila");
+// window.prompt("سن خود را وارد نمایید");
+
+console.log(window);
---
+++ sessions/T100-Working with DOM/S03-Window-Object-in-JavaScript/script.js
@@ -0,0 +1,6 @@
+console.log(document);
+console.dir(document);
+console.log(document.head);
+console.dir(document.head);
+console.log(document.body);
+console.dir(document.body);
