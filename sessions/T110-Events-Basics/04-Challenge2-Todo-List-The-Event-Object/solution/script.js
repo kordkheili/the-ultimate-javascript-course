4e9de4fa88df9149ac66873c71a24725c37cd2c2
Alireza-Office <mkalireza.ir@gmail.com>
2023-12-11 14:22:34 +0330

    05-Mouse-Events

--- sessions/T110-Events-Basics/04-Challenge2-Todo-List-The-Event-Object/solution/script.js
+++ sessions/T110-Events-Basics/04-Challenge2-Todo-List-The-Event-Object/solution/script.js
@@ -3,6 +3,12 @@ const input = document.getElementById("taskInput");
 const list = document.getElementById("tasksList");
 const notification = document.getElementById("notification");
 
+document.body.addEventListener("click", function (event) {
+  if (event.target.classList[0] === "delete") {
+    event.target.parentElement.remove();
+    notificationHandler();
+  }
+});
 addBtn.addEventListener("click", function () {
   addTask(input.value);
   input.value = "";
@@ -23,7 +29,19 @@ function addTask(task) {
 }
 function notificationHandler() {
   const tasks = document.querySelectorAll("#tasksList li");
-  if (tasks.length > 0) {
-    notification.style.display = "none";
-  }
+  console.log(tasks);
+  notification.style.display = tasks.length > 0 ? "none" : "block";
 }
+
+
+//! playground
+/* const btn = document.querySelector(".btn");
+console.log(btn);
+btn.addEventListener("click", function (event) {
+  console.log(event);
+  console.log(typeof event);
+}); */
+/* document.body.addEventListener("click", function (event) {
+  console.log("you clicked on body");
+  console.log(event.target);
+}); */
---
+++ sessions/T110-Events-Basics/05-Mouse-Events/phantom.js
@@ -0,0 +1,51 @@
+const logo = document.querySelector(".logo");
+
+//! methods
+const clickHandler = function (event) {
+  console.log("click event");
+};
+const dblclickHandler = function (event) {
+  console.log("dblclick event");
+};
+const rightClickHandler = function (event) {
+  console.log("rightClick event");
+};
+const mousedownHandler = function (event) {
+  console.log("mousedown event");
+};
+const mouseupHandler = function (event) {
+  console.log("mouseup event");
+};
+const wheelHandler = function (event) {
+  console.log("wheel event");
+};
+const mouseenterHandler = function (event) {
+  console.log("mouseenter event");
+};
+const mouseleaveHandler = function (event) {
+  console.log("mouseleave event");
+};
+const mousemoveHandler = function (event) {
+  console.log("mousemove event");
+};
+const dragstartHandler = function (event) {
+  console.log("dragstart event", event);
+};
+const dragendHandler = function (event) {
+  console.log("dragend event", event);
+};
+const dragHandler = function (event) {
+  console.log("drag event");
+};
+//! events
+logo.addEventListener("click", clickHandler);
+logo.addEventListener("dblclick", dblclickHandler);
+logo.addEventListener("contextmenu", rightClickHandler);
+logo.addEventListener("mousedown", mousedownHandler);
+logo.addEventListener("wheel", wheelHandler);
+logo.addEventListener("mouseenter", mouseenterHandler);
+logo.addEventListener("mousemove", mousemoveHandler);
+logo.addEventListener("mouseleave", mouseleaveHandler);
+logo.addEventListener("dragstart", dragstartHandler);
+logo.addEventListener("drag", dragHandler);
+logo.addEventListener("dragend", dragendHandler);
