29f3aa7300a05503222d60738339537723d03c28
Alireza-Office <mkalireza.ir@gmail.com>
2023-12-22 16:18:10 +0330

    11-Challenge4-(XY)-Removing-Event-Listeners

--- sessions/T110-Events-Basics/11-Challenge4-(XY)-Removing-Event-Listeners/solution/script.js
+++ sessions/T110-Events-Basics/11-Challenge4-(XY)-Removing-Event-Listeners/solution/script.js
@@ -3,18 +3,17 @@ const x = document.getElementById("result-x");
 const y = document.getElementById("result-y");
 const stop = document.getElementById("stop");
 const start = document.getElementById("start");
-function mouseoverHandler(e) {
+function mousemoveHandler(e) {
   x.innerText = e.offsetX;
   y.innerText = e.offsetY;
 }
-area.addEventListener("mousemove", mouseoverHandler);
 stop.addEventListener("click", function () {
-  area.removeEventListener("mousemove", mouseoverHandler);
+  area.removeEventListener("mousemove", mousemoveHandler);
   area.classList.remove("active");
-  x.innerText = "0";
-  y.innerText = "0";
+  x.innerText = 0;
+  y.innerText = 0;
 });
 start.addEventListener("click", function () {
-  area.addEventListener("mousemove", mouseoverHandler);
+  area.addEventListener("mousemove", mousemoveHandler);
   area.classList.add("active");
 });
