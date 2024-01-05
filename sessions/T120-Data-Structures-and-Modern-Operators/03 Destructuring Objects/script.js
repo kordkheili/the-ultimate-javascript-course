6b25736d7dad31e69f0a6e9f7a0ef2191fd60b46
Alireza-Office <mkalireza.ir@gmail.com>
2024-01-05 15:14:15 +0330

    03 Destructuring Objects

---
+++ sessions/T120-Data-Structures-and-Modern-Operators/03 Destructuring Objects/script.js
@@ -0,0 +1,18 @@
+const product = {
+  id: 10,
+  title: "HP Pavilion 15-DK1056WM",
+  description: "HP Pavilion 15-DK1056WM Gaming...",
+  price: 1099,
+  discountPercentage: 6.18,
+  rating: { rate: 4.3, count: 120 },
+  stock: 89,
+  brand: "HP Pavilion",
+  category: "laptops",
+  tags: ["Tech Product", "Laptop", "Gaming"],
+  thumbnail: "https://i.dummyjson.com/data/products/10/thumbnail.jpeg",
+  images: [
+    "https://i.dummyjson.com/data/products/10/1.jpg",
+    "https://i.dummyjson.com/data/products/10/2.jpg",
+    "https://i.dummyjson.com/data/products/10/3.jpg",
+  ],
+};
