26aede430d10ee276bfdc01a6037a9c9b6a94a23
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-03-01 15:59:39 +0330

    03-Primitive-vs-Reference-Types

---
+++ sessions/S11-Behind-the-Scenes-of-JavaScript/03-Primitive-vs-Reference-Types/script.js
@@ -0,0 +1,18 @@
+// ! Primitive Data Types
+const a = "Kordkheili"; // String
+const b = 1001; // Number
+const c = true; // Boolean
+const d = null; // null
+const e = undefined; // undefined
+const f = Symbol("foo"); // Symbol
+const g = BigInt(90071992547409991); // BigInt
+
+// ! Reference Data Type (Object)
+const x = {
+  firstName: "Alireza",
+  lastName: "Mohammadi Kordkheili",
+};
+const y = [1, 3, 5, 7, 9];
+const z = function () {
+  console.log("Hello World!");
+};
