e22dde32ad48e4344d951625941538ef69aef498
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-03-08 13:53:46 +0330

    10-Challenge5-toJalali

--- sessions/S12-Numbers-Math-Date-Intl-Timers/10-Challenge5-toJalali/script.js
+++
@@ -1,9 +0,0 @@
-function getDaysDiff(startDate, endDate) {
-  const diffTimestamp = Math.abs(endDate - startDate);
-  const diffInDays = diffTimestamp / 1000 / 60 / 60 / 24;
-  return diffInDays;
-}
-const startDate = new Date("2021-12-01"); // December 1, 2023
-const endDate = new Date("2024-01-01"); // January 1, 2024
-const daysDiff = getDaysDiff(startDate, endDate);
-console.log(daysDiff);
