bd3df4f5f63231361c47f5380f359380d6a358b3
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-03-09 15:07:53 +0330

    13-Challenge6-Countdown-Timer

--- sessions/S12-Numbers-Math-Date-Intl-Timers/13-Challenge6-Countdown-Timer/solution/script.js
+++ sessions/S12-Numbers-Math-Date-Intl-Timers/13-Challenge6-Countdown-Timer/solution/script.js
@@ -3,8 +3,9 @@ const btnResume = document.querySelector(".btn-resume");
 const btnPause = document.querySelector(".btn-pause");
 const btnReset = document.querySelector(".btn-reset");
 const timerDisplay = document.querySelector(".timer-display");
-
-let seconds;
+const startString = timerDisplay.innerText;
+const startArray = startString.split(":");
+let seconds = +startArray[0] * 60 + +startArray[1];
 let timerInterval;
 
 function intervalHandler() {
@@ -14,13 +15,6 @@ function intervalHandler() {
   const timer_minute_string = String(timer_minute).padStart(2, "0");
   const timer_second_string = String(timer_second).padStart(2, "0");
   timerDisplay.innerText = `${timer_minute_string}:${timer_second_string}`;
-}
-
-function init() {
-  const startString = timerDisplay.dataset.start;
-  const startArray = startString.split(":");
-  seconds = +startArray[0] * 60 + +startArray[1];
-  timerDisplay.innerText = startString;
 }
 
 function startHandler() {
@@ -48,7 +42,8 @@ function resumeHandler() {
 }
 
 function resetHandler() {
-  init();
+  timerDisplay.innerHTML = `<img src="loading.svg" />`;
+  seconds = +startArray[0] * 60 + +startArray[1];
   clearInterval(timerInterval);
   startHandler();
 }
@@ -57,5 +52,3 @@ btnStart.addEventListener("click", startHandler);
 btnResume.addEventListener("click", resumeHandler);
 btnPause.addEventListener("click", pauseHandler);
 btnReset.addEventListener("click", resetHandler);
-
-init();
