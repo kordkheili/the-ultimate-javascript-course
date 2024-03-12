6f42634e23f3e15b2f11c6eaf6a29cb914781fc1
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-03-12 12:57:07 +0330

    12-Timers-setTimeout-and-setInterval updated

--- sessions/S12-Numbers-Math-Date-Intl-Timers/12-Timers-setTimeout-and-setInterval/script.js
+++ sessions/S12-Numbers-Math-Date-Intl-Timers/12-Timers-setTimeout-and-setInterval/script.js
@@ -1,20 +1,14 @@
 //! setTimeout()
 const timer1 = setTimeout(function () {
-  console.log("timer1", "Hello World! 1");
+  console.log("timer1: ", "a message from timer 1");
 }, 3000);
 
-console.log("Hello World! 2");
+console.log("a message from outside of timer");
 
 //! setInterval()
 const timer2 = setInterval(function () {
-  console.log("timer2", new Date());
+  console.log("timer2: ", new Date());
 }, 1000);
-
-// //! setInterval() -> separate function
-// function timeLogger() {
-//   console.log(new Date());
-// }
-// const timer2 = setInterval(timeLogger, 1000);
 
 //! clearTimeout() , clearInterval()
 const btn1 = document.querySelector(".timer1Btn");
@@ -29,14 +23,20 @@ function clickHandler2() {
 }
 btn2.addEventListener("click", clickHandler2);
 
-//! setTimeout with input arguments
+//! setTimeout() with callback input arguments
 const timer3 = setTimeout(
   function (a, b, firstName) {
-    console.log("timer3", a + b);
-    console.log("timer3", `Hello ${firstName}`);
+    console.log("timer3: ", a + b);
+    console.log("timer3: ", `Hello ${firstName}`);
   },
   3000,
   2,
   3,
   "alireza"
 );
+
+//! setTimeout() -> separate function
+function timeLogger() {
+  console.log("timer4: ", "here is a message from timer4");
+}
+const timer4 = setTimeout(timeLogger, 1000);
