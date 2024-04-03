8f068ed91c17d6b1ad9a9805e4e87c4fef6341b2
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-04-03 14:59:54 +0330

    09-apply-call-bind-in-Practice

--- sessions/S13-Functions-Advanced/09-apply-call-bind-in-Practice/script.js
+++ sessions/S13-Functions-Advanced/09-apply-call-bind-in-Practice/script.js
@@ -1,13 +1,17 @@
 const user1 = {
   firstName: "Alireza",
   lastName: "Kordkheili",
-  introduce: function (job) {
-    console.log(`I'm ${this.firstName} ${this.lastName}, ${job}`);
+  introduce: function (job, age) {
+    console.log(`I'm ${this.firstName} ${this.lastName}, ${job} ${age}`);
   },
 };
+
 const user2 = {
   firstName: "Sara",
   lastName: "Razavi",
 };
-user1.introduce("Developer"); // I'm Alireza Kordkheili, Developer
-user1.introduce.apply(user2, ["Marketer"]); // I'm Sara Razavi, Marketer
+
+user1.introduce("Developer", 100);
+user1.introduce.apply(user2, ["Marketer", 34]);
+user1.introduce.call(user2, "Marketer", 53);
+user1.introduce.bind(user2, "Marketer", 15)();
