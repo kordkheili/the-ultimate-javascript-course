86d145d9dab758d30d622afbc4372b9b846d36db
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-04-03 15:13:48 +0330

    10-Immediately-Invoked-Function-Expressions-IIFE copy

---
+++ sessions/S13-Functions-Advanced/10-Immediately-Invoked-Function-Expressions-IIFE copy/script.js
@@ -0,0 +1,11 @@
+//! IIFE => Immediately Invoked Function Expression
+
+// Classic IIFE
+(function () {
+  console.log("I can only run once 😕");
+})();
+
+// Arrow IIFE
+(() => {
+  console.log("me too... 😥");
+})();
