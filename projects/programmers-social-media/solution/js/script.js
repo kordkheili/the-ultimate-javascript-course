beeb23ffd9a94a5feb4b4b2ed69c7c12530c645b
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-04-28 14:29:53 +0330

    programmers-social-media updated

---
+++ projects/programmers-social-media/solution/js/script.js
@@ -0,0 +1,22 @@
+const gradients = [
+  "bg-gradient-to-r from-indigo-500 to-purple-500",
+  "bg-gradient-to-r from-cyan-500 to-blue-500",
+  "bg-gradient-to-r from-cyan-800 to-gray-800",
+  "bg-gradient-to-r from-pink-500 to-yellow-500",
+  "bg-gradient-to-r from-green-400 to-blue-500",
+  "bg-gradient-to-r from-sky-400 to-emerald-600",
+];
+
+const users = [
+  {
+    firstName: "علیرضا",
+    lastName: "محمدی",
+    image: "images/profile-0.jpg",
+    job: "توسعه دهنده وب",
+    following: 272,
+    followers: 2000,
+    projects: 89,
+    salary: 73000000,
+    workHours: 49,
+  },
+];
