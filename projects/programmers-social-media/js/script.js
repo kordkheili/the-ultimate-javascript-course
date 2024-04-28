bdc8e654407047b85eac18da4954428268832f61
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-04-28 14:02:24 +0330

    programmers-social-media added

---
+++ projects/programmers-social-media/js/script.js
@@ -0,0 +1,19 @@
+const gradients = [
+  "from-indigo-500 via-purple-500",
+  "from-cyan-500 to-blue-500",
+  "from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%",
+  "from-pink-500 hover:to-yellow-500",
+  "from-green-400 to-blue-500",
+];
+
+const users = [
+  {
+    firstName: "علیرضا",
+    lastName: "محمدی",
+    image: "images/profile-0.jpg",
+    job: "توسعه دهنده وب",
+    following: 272,
+    followers: 2000,
+    projects: 89,
+  },
+];
