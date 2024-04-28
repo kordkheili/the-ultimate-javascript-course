4313849fc68fd7739f16fd5bf625a3d82e36a8b7
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-04-28 16:17:11 +0330

    working on programmers-social-media

--- projects/programmers-social-media/solution/js/script.js
+++ projects/programmers-social-media/solution/js/script.js
@@ -1,12 +1,4 @@
-const gradients = [
-  "bg-gradient-to-r from-indigo-500 to-purple-500",
-  "bg-gradient-to-r from-cyan-500 to-blue-500",
-  "bg-gradient-to-r from-cyan-800 to-gray-800",
-  "bg-gradient-to-r from-pink-500 to-yellow-500",
-  "bg-gradient-to-r from-green-400 to-blue-500",
-  "bg-gradient-to-r from-sky-400 to-emerald-600",
-];
-
+//! data
 const users = [
   {
     firstName: "علیرضا",
@@ -14,9 +6,230 @@ const users = [
     image: "images/profile-0.jpg",
     job: "توسعه دهنده وب",
     following: 272,
-    followers: 2000,
+    follower: 2000,
     projects: 89,
     salary: 73000000,
     workHours: 49,
   },
+  {
+    firstName: "نوید",
+    lastName: "نیکنام",
+    image: "images/profile-1.jpg",
+    job: "برنامه‌نویس ارشد بک‌اند",
+    following: 272,
+    follower: 1200,
+    projects: 6,
+    salary: 33000000,
+    workHours: 50,
+  },
+  {
+    firstName: "روژین",
+    lastName: "کریمی",
+    image: "images/profile-8w.jpg",
+    job: "برنامه‌نویس وب",
+    following: 50,
+    follower: 350,
+    projects: 55,
+    salary: 54000000,
+    workHours: 42,
+  },
+  {
+    firstName: "نازلی",
+    lastName: "موسوی",
+    image: "images/profile-5w.jpg",
+    job: "مهندس نرم افزار",
+    following: 1002,
+    follower: 20,
+    projects: 9,
+    salary: 37000000,
+    workHours: 49,
+  },
+  {
+    firstName: "هادی",
+    lastName: "کریمی",
+    image: "images/profile-4.jpg",
+    job: "کارشناس سئو",
+    following: 2720,
+    follower: 7050,
+    projects: 0,
+    salary: 46000000,
+    workHours: 49,
+  },
+  {
+    firstName: "پوریا",
+    lastName: "براتیان",
+    image: "images/profile-3.jpg",
+    job: "کارشناس تولید محتوا",
+    following: 9872,
+    follower: 101,
+    projects: 1,
+    salary: 52000000,
+    workHours: 56,
+  },
+  {
+    firstName: "ایمان",
+    lastName: "جلیلی",
+    image: "images/profile-6.jpg",
+    job: "گرافیست",
+    following: 65,
+    follower: 2000,
+    projects: 89,
+    salary: 31000000,
+    workHours: 30,
+  },
+  {
+    firstName: "پارسا",
+    lastName: "سلمانیان",
+    image: "images/profile-7.jpg",
+    job: "عکاسی و تولید محتوا",
+    following: 21,
+    follower: 2000,
+    projects: 20,
+    salary: 8100000,
+    workHours: 55,
+  },
+  {
+    firstName: "سهیل",
+    lastName: "نعمتی",
+    image: "images/profile-2.jpg",
+    job: "سرپرست فروش",
+    following: 50,
+    follower: 2000,
+    projects: 89,
+    salary: 36000000,
+    workHours: 44,
+  },
+  {
+    firstName: "مریم",
+    lastName: "علوی",
+    image: "images/profile-9w.jpg",
+    job: "تصویر بردار و تدوین‌گر",
+    following: 272,
+    follower: 2000,
+    projects: 50,
+    salary: 45000000,
+    workHours: 46,
+  },
+  {
+    firstName: "علی",
+    lastName: "سلطانیان",
+    image: "images/profile-10.jpg",
+    job: "مدیر خلاقیت",
+    following: 272,
+    follower: 2000,
+    projects: 32,
+    salary: 34000000,
+    workHours: 54,
+  },
 ];
+
+//! elements
+const userGrid = document.querySelector(".user-list");
+const statisticUsersEl = document.getElementById("statistic-users");
+const statisticSalaryEl = document.getElementById("statistic-salary");
+const statisticProjectsEl = document.getElementById("statistic-projects");
+const statisticWorkHoursEl = document.getElementById("statistic-work-hours");
+
+//! logic
+const usersHTML = users.map((user) => {
+  const template = `
+    <div
+      class="user-card w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 text-center p-2"
+    >
+      <div class="user-card__inner border rounded-lg overflow-hidden">
+        <div
+          class="user-card__head bg-gradient-to-r ${gradientGenerator()} w-full h-20"
+        ></div>
+        <div
+          class="user-card__body flex flex-col items-center p-4 border-t"
+        >
+          <div
+            class="user-card__image rounded-full overflow-hidden w-20 -mt-14 border-2 border-white"
+          >
+            <img class="w-100" src="${user.image}" />
+          </div>
+          <div class="user-card__user font-bold mt-2">
+            ${user.firstName} ${user.lastName}
+          </div>
+          <div class="user-card__job"></div>
+        </div>
+        <div class="user-card__footer flex py-2 border-t">
+          <div class="user-card__projects basis-1/3 border-l">
+            <div>${user.following}</div>
+            <div>Following</div>
+          </div>
+          <div class="user-card__follower basis-1/3 border-l">
+            <div>${user.follower}</div>
+            <div>Follower</div>
+          </div>
+          <div class="user-card__following basis-1/3">
+            <div>${user.projects}</div>
+            <div>Projects</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+  return template;
+});
+usersHTML.forEach((userHTML) => {
+  userGrid.insertAdjacentHTML("beforeend", userHTML);
+});
+statisticUsersEl.innerText = users.length;
+statisticSalaryEl.innerText = calcAverageSalary(users);
+statisticWorkHoursEl.innerText = calcAverageWorkHours(users) + " ساعت";
+statisticProjectsEl.innerText = `${projectsCounter(users)}+`;
+
+//! helper functions 👇🏻
+function randomGenerator(min, max) {
+  const random = min + Math.random() * (max - min);
+  const result = Math.round(random);
+  return result;
+}
+
+function gradientGenerator() {
+  const gradients = [
+    "from-red-500 to-orange-500",
+    "from-orange-500 to-amber-500",
+    "from-amber-500 to-yellow-500",
+    "from-yellow-500 to-lime-500",
+    "from-lime-500 to-green-500",
+    "from-green-500 to-emerald-500",
+    "from-emerald-500 to-teal-500",
+    "from-teal-500 to-cyan-500",
+    "from-cyan-500 to-sky-500",
+    "from-sky-500 to-blue-500",
+    "from-blue-500 to-indigo-500",
+    "from-indigo-500 to-violet-500",
+    "from-violet-500 to-purple-500",
+    "from-purple-500 to-fuchsia-500",
+    "from-fuchsia-500 to-pink-500",
+    "from-pink-500 to-rose-500",
+  ];
+  const randomIndex = randomGenerator(0, gradients.length - 1);
+  return gradients[randomIndex];
+}
+function calcAverageSalary(usersArray) {
+  const sum = usersArray.reduce((accumulator, current) => {
+    return accumulator + current.salary;
+  }, 0);
+  const avg = sum / usersArray.length;
+  const roundedAvg = Math.ceil(avg / 10000) * 10000;
+  const result = Intl.NumberFormat("en-US").format(roundedAvg);
+  return result;
+}
+function calcAverageWorkHours(usersArray) {
+  const sum = usersArray.reduce((accumulator, current) => {
+    return accumulator + current.workHours;
+  }, 0);
+  const avg = sum / usersArray.length;
+  const roundedAvg = Math.ceil(avg * 10) / 10;
+  const result = Intl.NumberFormat("en-US").format(roundedAvg);
+  return result;
+}
+function projectsCounter(usersArray) {
+  const sum = usersArray.reduce((accumulator, current) => {
+    return accumulator + current.projects;
+  }, 0);
+  return sum;
+}
