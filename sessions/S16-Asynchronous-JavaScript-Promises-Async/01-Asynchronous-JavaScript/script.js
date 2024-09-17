85fcb7ff6155ae3c63b4dfbad733e55c8dc843b5
Alireza Mohammadi Kordkheili <mkalireza.ir@gmail.com>
2024-09-17 11:48:52 +0330

    01-Asynchronous-JavaScript

---
+++ sessions/S16-Asynchronous-JavaScript-Promises-Async/01-Asynchronous-JavaScript/script.js
@@ -0,0 +1,17 @@
+//! synchronous
+// console.log("Salam");
+// alert("Mozaahem😁");
+// console.log("Aleyke Salam!");
+
+//! asynchronous
+console.log("Salam");
+
+setTimeout(function () {
+  console.log("🔥async🔥");
+}, 3000);
+
+setTimeout(function () {
+  console.log("🍕");
+}, 1000);
+
+console.log("Aleyke Salam!");
